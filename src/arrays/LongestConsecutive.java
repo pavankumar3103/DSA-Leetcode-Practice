72d7e0c469756705e51f856ab8ae6bd94d14540d
Pavan Kumar P <pavan.kumar310399@gmail.com>
2026-07-02 17:23:39 +0100

    Add: LongestConsecutive(Medium, Arrays, HashTable)

---
+++ src/arrays/LongestConsecutive.java
@@ -0,0 +1,28 @@
+package arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutive {
+    public int longestConsecutive(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
+        }
+        int longestStreak = 0;
+        for(int num: set){
+            if(!set.contains(num - 1)){
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(num + 1)) {
+                    currentNum += 1;
+                    currentStreak++;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+}
+
